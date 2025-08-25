87c89e8da1e899d2af6d40cd8cc0b95f6625cde6
NagaSistemas <nagasistemas@gmail.com>
2025-08-25 00:37:17 -0300

    fix: Porta dinâmica Railway

--- main.py
+++ main.py
@@ -69,4 +69,5 @@ if os.path.exists(FRONTEND_DIR):
     app.mount("/", StaticFiles(directory=FRONTEND_DIR, html=True), name="static")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
